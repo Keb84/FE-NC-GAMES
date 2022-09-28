b86910e4d124cd9809bee8e2d977c75c5306240b
Alison Rooney <robandroo@Alisons-Air.broadband>
2022-09-28 12:33:00 +0100

    ticket 7 completed nc-games

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,17 +1,31 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { reviewByID } from "../utils/api"
+import { voteOnReview } from "../utils/api"
 
 
 function ReviewCard () {
 
     const [singleReview, setSingleReview] =useState({})
+    const [incVotes, setIncVotes] = useState(null)
+    const [hasVoted, setHasVoted] = useState(false)
+    const [voteMessage, setVoteMessage] = useState('')
 
-        const {review_id} = useParams()
+    const handleIncVotes = (review_id) => {
+        if (!hasVoted){
+            voteOnReview(review_id)
+            setIncVotes((currVotes) => 
+            currVotes + 1
+            
+            )
+            setHasVoted(true)
+        }else {
+            setVoteMessage('Sorry you can only vote once')
+        }
+        
+    }
 
-
-
-
+    const {review_id} = useParams()
 
     useEffect(() => {
         reviewByID(review_id)
@@ -19,16 +33,18 @@ function ReviewCard () {
         .then(({reviews}) => {
 
             setSingleReview(reviews)
+            setIncVotes(reviews.votes)
         })      
     }, [review_id])
+
     return (
         <section>
         <h2>Username : {singleReview.owner}</h2>
         <p>Review title : {singleReview.title}</p>
         <p>Game designer : {singleReview.designer}</p>
         <p>{singleReview.review_body}</p>
+        <button onClick={() => handleIncVotes(singleReview.review_id)}>{incVotes}<span aria-label="votes for this review"> 👍</span></button><p>{voteMessage}</p>
         <p>Game category : {singleReview.category}</p>
-        <p>Votes : {singleReview.votes}</p>
         <img src={singleReview.review_img_url} alt={singleReview.owner}/>
 
         </section>
